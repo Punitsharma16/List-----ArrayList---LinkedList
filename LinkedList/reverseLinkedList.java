db0237f0abeaf081295516f6aa41b1ed2fa37ba8
Punitsharma16 <punitsharma9034908543@gmail.com>
2023-07-26 12:58:30 +0530

    Lists

---
+++ LinkedList/reverseLinkedList.java
@@ -0,0 +1,28 @@
+package LinkedList;
+
+public class reverseLinkedList {
+    Node head;
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public void reverse(){
+        if(head== null || head.next == null){
+            return;
+        }
+        Node prevNode = head;
+        Node currNode = head.next;
+        while(currNode != null) {
+            Node nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        head.next = null;
+        head = prevNode;
+    }
+}
